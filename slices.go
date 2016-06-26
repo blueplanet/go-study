8f317d1a546c007a039955bc9ac5ce3d2237cc98
blueplanet <erguolinge@gmail.com>
2016-06-26 17:09:06 +0900

    slice 2

--- slices.go
+++ slices.go
@@ -7,4 +7,20 @@ func main() {
 
   s := permes[1:4]
   fmt.Println(s)
+
+  names := [4]string{
+		"a",
+		"b",
+		"c",
+		"d",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
