2c1ac2b71d0d646fb63979f6026b93f08ff3e98f
blueplanet <erguolinge@gmail.com>
2016-07-11 06:30:41 +0800

    map exercise

---
+++ map-exercise.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+  "strings"
+)
+
+func WordCount(s string) map[string]int {
+  counts := make(map[string]int)
+
+  for _, word := range strings.Fields(s) {
+    counts[word] += 1
+  }
+
+	return counts
+}
+
+func main() {
+	wc.Test(WordCount)
+}
